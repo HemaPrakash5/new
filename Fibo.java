c6fbfe5e170807109d6ce6e5b104e2dd12b40047
HemaPrakash5 <neelihemaprakash@gmail.com>
2026-05-16 10:49:47 +0530

    Add files via upload

---
+++ Fibo.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Fibo{
+    public static void main(String[]args){
+        int n=6;
+        System.out.println(fibo(n));
+    }
+    static int fibo(int n){
+        if(n<2){
+            return n;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
+}
